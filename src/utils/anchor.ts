a7cf9e6a9d8887bf6fae2625630ebeb7c46aa028
Thomas Eckhardt <mail@thomaseckhardt.com>
2024-09-05 00:38:36 +0200

    Fix anchors

--- src/utils/anchor.ts
+++ src/utils/anchor.ts
@@ -1,12 +1,17 @@
 import { slugify } from './slug'
 
-type AnchorLink = {
-  id: string
-  href: string
-  label: string
-}
+type AnchorLink =
+  | {
+      id: string
+      href: string
+      label: string
+    }
+  | undefined
 
 export const getAnchor = (anchor: string = ''): AnchorLink => {
+  if (!anchor) {
+    return undefined
+  }
   const id = slugify(anchor) ?? ''
   const href = `#${id}`
   const label = anchor
